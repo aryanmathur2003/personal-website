6d25a8c5a539f4409366c9713a11e42533e524c6
Aryan Mathur <65492352+aryanmathur2003@users.noreply.github.com>
2024-06-03 12:42:55 -0700

    Fixes + New Deployment

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -4,8 +4,7 @@ export default function ContactMe() {
         <div>
           <p className="sub--title">Get In Touch</p>
           <h2>Contact Me</h2>
-          <p className="text-lg">
-            
+          <p className="text-lg">           
           </p>
         </div>
         <form className="contact--form--container">
