9883ce8b66bf45d99b2f5a692e7329a1e84f31c5
Aryan Mathur <65492352+aryanmathur2003@users.noreply.github.com>
2025-01-02 00:07:26 -0800

    HostingFixes

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -9,7 +9,7 @@ export default function AboutMe() {
             <p className="section--title"></p>
             <h1 className="skills-section--heading">About Me</h1>
             <p className="hero--section-description">
-            As a computer science enthusiast currently pursuing my education at Purdue University, I am passionate about driving positive change within communities through innovative solutions. Proficient in Python, JavaScript, C, C++, Java, and Typescript, my skills extend to back-end development and SQL. I leverage frameworks such as React.js, Node.js, React Native, Express.js, and Django. I am committed to using my technical expertise to contribute meaningfully to the dynamic field of computer science, with the goal of enhancing lives and communities through advanced technology. 
+            As a computer science enthusiast with a BS in CS from Purdue University, I am passionate about driving positive change within communities through innovative solutions. Proficient in Python, JavaScript, C, C++, Java, and Typescript, my skills extend to back-end development and SQL. I leverage frameworks such as React.js, Node.js, React Native, Express.js, and Django. I am committed to using my technical expertise to contribute meaningfully to the dynamic field of computer science, with the goal of enhancing lives and communities through advanced technology. 
             </p>
             <p className="hero--section-description">
             
