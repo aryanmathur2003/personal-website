b90a9f403d30336bd6e9d69d9dff385f5909ff1b
Aryan Mathur <65492352+aryanmathur2003@users.noreply.github.com>
2025-01-08 23:06:42 -0800

    About Me fixes

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -9,7 +9,7 @@ export default function AboutMe() {
             <p className="section--title"></p>
             <h1 className="skills-section--heading">About Me</h1>
             <p className="hero--section-description">
-            I'm a forward-thinking computer scientist with a BS in Computer Science from Purdue University, where I honed a diverse skill set spanning Python, JavaScript, C, C++, Java, and TypeScript. My passion lies in building cutting-edge solutions that make a real impact—whether that's optimizing back-end systems, crafting sleek front-end experiences, or architecting full-stack applications from scratch. Armed with frameworks like React.js, Node.js, React Native, Express.js, and Django, I thrive on leveraging technology to uplift communities and drive positive change. At heart, I'm a creative problem solver who’s always seeking the next challenge, dedicated to pushing boundaries and transforming bold ideas into tangible results.
+            I'm a forward-thinking computer scientist with a BS in Computer Science from Purdue University, where I honed a diverse skill set spanning Python, JavaScript, C, C++, Java, and TypeScript. My passion lies in building cutting-edge solutions that make a real impact—whether that's optimizing back-end systems, crafting sleek front-end experiences, or architecting full-stack applications from scratch. Armed with frameworks like React.js, Node.js, React Native, Express.js, and Django, I thrive on leveraging technology to uplift communities and drive positive change. At heart, I'm a creative problem solver who is always seeking the next challenge, dedicated to pushing boundaries and transforming bold ideas into tangible results.
             </p>
             <p className="hero--section-description">
             
