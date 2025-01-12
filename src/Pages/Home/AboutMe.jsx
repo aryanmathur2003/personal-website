dc4e75dd6224e8f21e22bfcaef013fb5977e8b00
Aryan Mathur <65492352+aryanmathur2003@users.noreply.github.com>
2025-01-11 21:05:03 -0800

    AboutMeChanges

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -1,21 +1,21 @@
 export default function AboutMe() {
-    return (
-      <section id="AboutMe" className="about--section">
-        <div className="about--section--img">
-          <img src="./img/about-me.png" alt="About Me" />
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title"></p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            I'm a forward-thinking computer scientist with a BS in Computer Science from Purdue University, where I developed a diverse skill set in Python, JavaScript, C, C++, Java, and TypeScript. My passion lies in creating cutting-edge solutions that make a real-world impact—whether optimizing back-end systems, crafting sleek front-end experiences, or architecting full-stack applications.
+          </p>
+          <p className="hero--section-description">
+            With experience in frameworks like React.js, Node.js, React Native, Express.js, and Django, I thrive on leveraging technology to uplift communities and drive positive change. I'm a creative problem solver, always seeking new challenges and dedicated to transforming bold ideas into tangible results.
+          </p>
         </div>
-        <div className="hero--section--content--box about--section--box">
-          <div className="hero--section--content">
-            <p className="section--title"></p>
-            <h1 className="skills-section--heading">About Me</h1>
-            <p className="hero--section-description">
-            I'm a forward-thinking computer scientist with a BS in Computer Science from Purdue University, where I honed a diverse skill set spanning Python, JavaScript, C, C++, Java, and TypeScript. My passion lies in building cutting-edge solutions that make a real impact—whether that's optimizing back-end systems, crafting sleek front-end experiences, or architecting full-stack applications from scratch. Armed with frameworks like React.js, Node.js, React Native, Express.js, and Django, I thrive on leveraging technology to uplift communities and drive positive change. At heart, I'm a creative problem solver who is always seeking the next challenge, dedicated to pushing boundaries and transforming bold ideas into tangible results.
-            </p>
-            <p className="hero--section-description">
-            
-            </p>
-          </div>
-        </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
+}
